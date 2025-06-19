70de9d84216f105b8b95ded1a2c3abd725929f5a
Jason McCallister <jason@mccallister.dev>
2025-06-18 21:17:10 -0400

    Add basic tests
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- src/test/utils/executable.simple.test.ts
+++ src/test/utils/executable.simple.test.ts
@@ -1,17 +1,17 @@
 import * as assert from 'assert';
 import { exists } from '../../utils/executable';
 
-suite('Executable Utils Test Suite', () => {
-	test('exists function should be defined', () => {
+describe('Executable Utils Test Suite', () => {
+	it('exists function should be defined', () => {
 		assert.ok(typeof exists === 'function');
 	});
 
-	test('exists should return false for non-existent command', async () => {
+	it('exists should return false for non-existent command', async () => {
 		const result = await exists('this-command-definitely-does-not-exist-12345');
 		assert.strictEqual(result, false);
 	});
 
-	test('exists should return true for a command that exists (ls)', async () => {
+	it('exists should return true for a command that exists (ls)', async () => {
 		const result = await exists('ls');
 		assert.strictEqual(result, true);
 	});
