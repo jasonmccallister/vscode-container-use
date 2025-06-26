eacb47a777081eac12cfd31ec9fbe4fabca22181
Jason McCallister <jason@mccallister.dev>
2025-06-26 01:10:00 -0400

    fix the tests to use mocha
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,34 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+// import * as myExtension from '../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+describe('Extension Test Suite', () => {
+	before(() => {
+		vscode.window.showInformationMessage('Start all tests.');
+	});
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	it('should find expected values in array using indexOf', () => {
+		// Test negative cases - values not in array should return -1
+		const testArray = [1, 2, 3];
+		const expectedIndex = -1;
+		
+		assert.strictEqual(expectedIndex, testArray.indexOf(5));
+		assert.strictEqual(expectedIndex, testArray.indexOf(0));
+	});
+
+	it('should find expected values in array using indexOf (positive cases)', () => {
+		// Test positive cases - values in array should return correct index
+		const testArray = [1, 2, 3];
+		
+		assert.strictEqual(0, testArray.indexOf(1));
+		assert.strictEqual(1, testArray.indexOf(2));
+		assert.strictEqual(2, testArray.indexOf(3));
+	});
+
+	// Add a more comprehensive test for future extension functionality
+	it('should have vscode API available', () => {
+		assert.ok(vscode, 'vscode API should be available');
+		assert.ok(vscode.window, 'vscode.window should be available');
+		assert.ok(vscode.commands, 'vscode.commands should be available');
 	});
 });
